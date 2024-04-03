4e10b36f40cbcecde3f419989fe0688e9a1cd179
10soa <soaornella5@gmail.com>
2024-04-03 20:54:53 +0300

    Création du projet

---
+++ src/main/java/tp2/prg/jsf/soa/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package tp2.prg.jsf.soa;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
